eef3b95f880db0f9f13883e1a63065c3bccf2e4c
Alexandre <oger.alexandre@gmail.com>
2014-06-08 19:53:31 +0200

    added method chaining and open/close method

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var scrollilol = (function(window) {
 
     var elt,
         currentPosition,
-        prevPosition = startOffset = 0,
+        prevPosition = startOffset = i = 0,
         throttleRate = 250;
 
     var getScrollObject = function(_elementId) {
@@ -31,19 +31,27 @@ var scrollilol = (function(window) {
 
     var hasClass = function(element, cls) {
         return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-    }
+    };
+
+    var open = function() {
+        if(hasClass(elt,"hideit")) {
+            elt.className = elt.className.replace(new RegExp('(\\s|^)hideit(\\s|$)'),' ').replace(/^\s+|\s+$/g, '');
+        }
+    };
+
+    var close = function() {
+        if(!hasClass(elt,"hideit")) {
+            elt.className += " hideit";
+        }
+    };
 
     var scrollController = function() {
         getCurrentPosition();
         //elt.classList not available in "old" browsers : http://caniuse.com/classlist
         if(window.pageXOffset === 0 && currentPosition > startOffset && prevPosition < currentPosition && currentPosition > 0) {
-            if(!hasClass(elt,"hideit")) {
-                elt.className += " hideit";
-            }
+            close();
         } else if(currentPosition > startOffset && currentPosition < document.body.clientHeight - window.innerHeight) {
-            if(hasClass(elt,"hideit")) {
-                elt.className = elt.className.replace(new RegExp('(\\s|^)hideit(\\s|$)'),' ').replace(/^\s+|\s+$/g, '');
-            }
+            open();
         }
         prevPosition = currentPosition;
     };
@@ -79,11 +87,23 @@ var scrollilol = (function(window) {
         init: function(_elementId, _startOffset, _throttleRate) {
             startOffset = _startOffset || 0;
             throttleRate = _throttleRate;
-            
+
             getScrollObject(_elementId) || console.log('Scrollilol : error >> element Id not found');
             listenScroll() || console.log('Scrollilol : error >> can\'t listenning the element');
+            
+            return this;
+        },
+        open: function(fn) {
+            open();
+            if(typeof fn !== 'undefined') fn();
+            return this;
+        },
+        close: function(fn) {
+            close();
+            if(typeof fn !== 'undefined') fn();
+            return this;
         }
     };
 })(window);
  
-scrollilol.init('scrollilol', 0, 100);
+scrollilol.init('scrollilol', 0, 100).close(function(){/* test */});
